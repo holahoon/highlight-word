da11225ac69b1e6a63009c33fc60f7e05f38c0ab
David Kim <david.kim@luckie.com>
2020-03-05 19:27:57 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createElement } from "react";
 
-function App() {
+export default function App() {
+  const [inputValue, setInputValue] = useState({
+    findWord: ""
+  });
+  const [sampleParagraph, setSampleParagraph] = useState(
+    "The German automaker's refreshed logo ditches the black ring for a transparent circle. The rest of it, including the typeface, has a flatter and more modern look. The blue and white emblem inside the ring remains."
+  );
+  const [showWord, setShowWord] = useState([]);
+
+  useEffect(() => {
+    findTheWord(inputValue.findWord);
+  }, [inputValue.findWord]);
+
+  const onChangeHandler = e => {
+    setInputValue({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const findTheWord = findWord => {
+    const splittedParagraph = sampleParagraph.split(" ");    
+    const wordFound = [];
+
+    splittedParagraph.forEach((word, i) => {
+      const regexWord = word.match(/[a-z]+|\d+/gi).join("");
+      if (regexWord === findWord) {
+        const spanWord = createElement(
+          "span",
+          { style: { color: "dodgerBlue" } },
+          word
+        );
+        wordFound.push(spanWord);
+      } else {
+        wordFound.push(word);
+      }
+      setShowWord(wordFound);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h3>Find word: {inputValue.findWord}</h3>
+        <form>
+          <input
+            type={"text"}
+            name={"findWord"}
+            value={inputValue.text}
+            onChange={e => onChangeHandler(e)}
+          />
+        </form>
+
+        <br />
+        <br />
+        <div>
+          {showWord.map(word => (
+            <span>{word} </span>
+          ))}
+          {/* {showWord.map(word => word) } */}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
